3b6589deb319b7a852013af5e6239a7123a7cd41
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-05-08 09:39:34 +0530

    Add files via upload

---
+++ Event delegation/app.js
@@ -0,0 +1,13 @@
+let btn=document.querySelectorAll('button');
+
+// for (let i=0;i<btn.length;i++){
+//     btn[i].addEventListener('click',function(e){
+//         console.log('click')
+//     })
+// }
+
+let parent=document.querySelector('.parent');
+
+parent.addEventListener('click',function(){
+    console.log('clicked');
+})
